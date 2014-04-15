d153385bbdecf5354a874218edc6d94a2075849c
vagrant <vagrant@kellogg-dev-box>
2014-04-15 13:42:10 +0000

    Generate the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bridesmaid::Application.config.secret_key_base = '8d332afc15da1e0bcfd15d29787cee652049021ec340b8ca9dcee3d010d25e8c0344a5aa615d98c1a892399868ca56873b34966a7d8df10a2fc17bb7c9513e1c'
